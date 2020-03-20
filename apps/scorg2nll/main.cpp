27e9da17c312000772c0802b1d4d9ea21827269c
Luca <luca.scarabello@gmail.com>
2020-03-20 09:34:38 +0100

    License and Copyright update
    GNU Affero General Public License for modules
    GNU LESSER GENERAL PUBLIC LICENSE for libraries
    Copyright SED - Swiss Seismological Service at ETH Zuerich

--- apps/scorg2nll/main.cpp
+++ apps/scorg2nll/main.cpp
@@ -1,10 +1,18 @@
 /***************************************************************************
-* Copyright (C) 2010 by Jan Becker, gempa GmbH                             *
-* EMail: jabe@gempa.de                                                     *
-*                                                                          *
-* This code has been developed for the SED/ETH Zurich and is               *
-* released under the SeisComP Public License.                              *
-***************************************************************************/
+ *   Copyright (C) by ETHZ/SED                                             *
+ *                                                                         *
+ * This program is free software: you can redistribute it and/or modify    *
+ * it under the terms of the GNU Affero General Public License as published*
+ * by the Free Software Foundation, either version 3 of the License, or    *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU Affero General Public License for more details.                     *
+ *                                                                         *
+ *   Developed by gempa GmbH                                               *
+ ***************************************************************************/
 
 #define SEISCOMP_COMPONENT Org2NLL
 
